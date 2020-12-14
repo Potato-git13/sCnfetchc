a02a5b9d50ed809cc455945864d969cf75545808
Potato Man <lanveble08@gmail.com>
2020-12-14 08:42:16 +0100

    Removed getting host from the main file

--- src/main.c
+++ src/main.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
 #include <sys/sysinfo.h>
 
 extern const char* whoami();
 extern const char* getDistro();
 extern const char* getShell();
+extern const char* getHost();
 
 int main(){
-	char hostBuffer[256];
 	struct sysinfo sysinfostruct;
     int min;
     int hour;
-
-	// Get the host name; store it in hostBuffer
-    gethostname(hostBuffer, sizeof(hostBuffer));
 	
 	if(! sysinfo (&sysinfostruct)){
         min = (sysinfostruct.uptime / 60) % 60;
@@ -23,7 +19,7 @@ int main(){
 
 	printf("\n");
 	printf("User   : %s\n", whoami());
-	printf("Host   : %s\n", hostBuffer);
+	printf("Host   : %s\n", getHost());
 	printf("Distro : %s\n", getDistro());
 	printf("Uptime : %dh, %dm\n", hour, min);
 	printf("Shell  : %s\n", getShell());
